1fd6da34fa6e5a5ac296500688b154bdf8c37126
Matt Ford <matthew.ford@schneider-electric.com>
2017-11-03 13:49:43 -0400

    First commit, added simple function

---
+++ ScratchSpace/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let rec printHelloWorld n = 
+    match n with 
+    | 0 -> "\n"
+    | i when i > 0 -> "Hello World\n" + printHelloWorld (i-1)
+    | _ -> failwith "Number must be positive"
+
+[<EntryPoint>]
+let main argv = 
+    let a = System.Console.ReadLine() |> int
+    printfn "%s" (printHelloWorld a)
+    0 // return an integer exit code
