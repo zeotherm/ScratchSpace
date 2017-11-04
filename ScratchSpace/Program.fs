607c3bc4ef942c866f56673ec05f2c7d48bec8cb
Matt Ford <zeotherm@gmail.com>
2017-11-03 20:19:43 -0400

    Further work on a problem

--- ScratchSpace/Program.fs
+++ ScratchSpace/Program.fs
@@ -1,4 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
 let rec printHelloWorld n = 
@@ -7,8 +9,18 @@ let rec printHelloWorld n =
     | i when i > 0 -> "Hello World\n" + printHelloWorld (i-1)
     | _ -> failwith "Number must be positive"
 
+let printList l = 
+    l |> Seq.iter (printf "%A\n")
+
+let readListFromInput() = 
+    let rec readHelper entries = 
+        match Console.ReadLine() with 
+        | "" | null -> List.rev entries
+        | e -> readHelper ((e |> int) :: entries)
+    readHelper []
+
 [<EntryPoint>]
 let main argv = 
-    let a = System.Console.ReadLine() |> int
-    printfn "%s" (printHelloWorld a)
+    let N = System.Console.ReadLine() |> int
+    readListFromInput() |> List.collect (fun x -> List.replicate N x) |> printList
     0 // return an integer exit code
